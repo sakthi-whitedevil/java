d6b5882d0411f6464adf13883234b567ca470bc9
saktheeswaran <ssaktheeswaran25@gamil.com>
2022-11-09 16:04:44 +0530

    commit

---
+++ ATM/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+public class Automation
+    {
+        public void display()
+        {
+            do
+            {
+                Console.WriteLine("**************");
+                Console.WriteLine("WELCOME TO ATM");
+                Console.WriteLine("**************");
+                
+            } while()
+        }
+    }
+
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
